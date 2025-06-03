51e88deec99343a0b1e8417eee0758d5923a54f8
Rumilesq <rumilesquillo9@gmail.com>
2025-06-03 22:56:28 +0800

    Add project files.

---
+++ GROUP10-ONLINEDONATION/LoginForm2.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+<Global.System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1726")> _
+Partial Class LoginForm2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+    Friend WithEvents UsernameLabel As System.Windows.Forms.Label
+    Friend WithEvents PasswordLabel As System.Windows.Forms.Label
+    Friend WithEvents UsernameTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents PasswordTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents OK As System.Windows.Forms.Button
+    Friend WithEvents Cancel As System.Windows.Forms.Button
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        UsernameLabel = New Label()
+        PasswordLabel = New Label()
+        UsernameTextBox = New TextBox()
+        PasswordTextBox = New TextBox()
+        OK = New Button()
+        Cancel = New Button()
+        LoginText = New Label()
+        SuspendLayout()
+        ' 
+        ' UsernameLabel
+        ' 
+        UsernameLabel.Location = New Point(217, 24)
+        UsernameLabel.Name = "UsernameLabel"
+        UsernameLabel.Size = New Size(220, 23)
+        UsernameLabel.TabIndex = 0
+        UsernameLabel.Text = "&User name"
+        UsernameLabel.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' PasswordLabel
+        ' 
+        PasswordLabel.Location = New Point(217, 81)
+        PasswordLabel.Name = "PasswordLabel"
+        PasswordLabel.Size = New Size(220, 23)
+        PasswordLabel.TabIndex = 2
+        PasswordLabel.Text = "&Password"
+        PasswordLabel.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' UsernameTextBox
+        ' 
+        UsernameTextBox.Location = New Point(217, 50)
+        UsernameTextBox.Name = "UsernameTextBox"
+        UsernameTextBox.Size = New Size(220, 23)
+        UsernameTextBox.TabIndex = 1
+        ' 
+        ' PasswordTextBox
+        ' 
+        PasswordTextBox.Location = New Point(217, 107)
+        PasswordTextBox.Name = "PasswordTextBox"
+        PasswordTextBox.PasswordChar = "*"c
+        PasswordTextBox.Size = New Size(220, 23)
+        PasswordTextBox.TabIndex = 3
+        ' 
+        ' OK
+        ' 
+        OK.Location = New Point(197, 161)
+        OK.Name = "OK"
+        OK.Size = New Size(94, 23)
+        OK.TabIndex = 4
+        OK.Text = "&OK"
+        ' 
+        ' Cancel
+        ' 
+        Cancel.DialogResult = DialogResult.Cancel
+        Cancel.Location = New Point(300, 161)
+        Cancel.Name = "Cancel"
+        Cancel.Size = New Size(94, 23)
+        Cancel.TabIndex = 5
+        Cancel.Text = "&Cancel"
+        ' 
+        ' LoginText
+        ' 
+        LoginText.Font = New Font("Sitka Small", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        LoginText.Location = New Point(27, 52)
+        LoginText.Name = "LoginText"
+        LoginText.Size = New Size(139, 78)
+        LoginText.TabIndex = 6
+        LoginText.Text = "Login"
+        LoginText.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' LoginForm2
+        ' 
+        AcceptButton = OK
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        CancelButton = Cancel
+        ClientSize = New Size(649, 230)
+        Controls.Add(LoginText)
+        Controls.Add(Cancel)
+        Controls.Add(OK)
+        Controls.Add(PasswordTextBox)
+        Controls.Add(UsernameTextBox)
+        Controls.Add(PasswordLabel)
+        Controls.Add(UsernameLabel)
+        FormBorderStyle = FormBorderStyle.FixedDialog
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "LoginForm2"
+        SizeGripStyle = SizeGripStyle.Hide
+        StartPosition = FormStartPosition.CenterParent
+        Text = "LoginForm2"
+        ResumeLayout(False)
+        PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LoginText As Label
+
+End Class
